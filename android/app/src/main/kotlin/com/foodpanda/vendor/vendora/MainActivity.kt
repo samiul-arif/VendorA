2df7cb8c836440c584c6fc058823c80e9780185e
samiul-arif <187525208+samiul-arif@users.noreply.github.com>
2026-08-31 22:09:49 +0600

    chore: exclude platform runners and auto-generated build files from git

--- android/app/src/main/kotlin/com/foodpanda/vendor/vendora/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.foodpanda.vendor.vendora
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Vendor App smoke test', () {
-    expect(true, isTrue);
-  });
-}
